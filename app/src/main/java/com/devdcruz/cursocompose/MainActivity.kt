fa2b99173b4b38bd565cf37d86e06efa52ce0108
DevDCruz <juandecruzsan@gmail.com>
2024-04-11 13:59:25 +0200

    spacers

--- app/src/main/java/com/devdcruz/cursocompose/MainActivity.kt
+++ app/src/main/java/com/devdcruz/cursocompose/MainActivity.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.devdcruz.cursocompose.ui.theme.CursoComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,6 +44,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyColumn() {
     Column(modifier = Modifier.fillMaxSize()) {
+        MySpacerVertical(size = 5)
         Box(
             modifier = Modifier
                 .fillMaxWidth()
@@ -48,11 +53,13 @@ fun MyColumn() {
         ) {
             Text(text = "Ejemplo 1")
         }
+        MySpacerVertical(size = 5)
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f)
         ) {
+            MySpacerHorizontal(size = 5)
             Box(
                 modifier = Modifier
                     .fillMaxHeight()
@@ -61,6 +68,9 @@ fun MyColumn() {
             ) {
                 Text(text = "Ejemplo 2")
             }
+
+            MySpacerHorizontal(size = 5)
+
             Box(
                 modifier = Modifier
                     .fillMaxHeight()
@@ -69,7 +79,11 @@ fun MyColumn() {
             ) {
                 Text(text = "Ejemplo 3")
             }
+            MySpacerHorizontal(size = 5)
         }
+
+        MySpacerVertical(size = 5)
+
         Box(
             modifier = Modifier
                 .fillMaxWidth()
@@ -78,8 +92,23 @@ fun MyColumn() {
         ) {
             Text(text = "Ejemplo 4")
         }
+        MySpacerVertical(size = 5)
     }
 
+}
+
+@Composable
+fun MySpacerVertical(size : Int) {
+    Spacer(modifier = Modifier
+        .height(size.dp)
+        .background(Color.Black))
+}
+
+@Composable
+fun MySpacerHorizontal(size : Int) {
+    Spacer(modifier = Modifier
+        .width(size.dp)
+        .background(color = Color.Black))
 }
 
 @Preview(showBackground = true)
